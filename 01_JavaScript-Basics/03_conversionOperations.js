7e8a063c96efed99c20c46f04e147c2b090b4902
aaftabgit <136881222+aaftabgit@users.noreply.github.com>
2024-01-09 06:44:10 +0000

    conversionOperations

---
+++ 01_JavaScript-Basics/03_conversionOperations.js
@@ -0,0 +1,67 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLogedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLogedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true => false
+// "" => false
+// "hello" => true
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// **************** Operations *************************
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue)
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "hi"
+let str2 = " hello"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true);
+// console.log(true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+
+// link to study  *********
