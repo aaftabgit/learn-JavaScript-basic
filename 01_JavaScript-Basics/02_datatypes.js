1db383f000d868719fefe0385977f48907f6ebdf
aaftabgit <136881222+aaftabgit@users.noreply.github.com>
2024-01-09 06:43:31 +0000

    datatypes

---
+++ 01_JavaScript-Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert (3 + 3) // we are using node js ,not browser
+
+console.log(3 
+    +
+    3 ) //code readability high 
+
+console.log("hello")    
+
+// datatype...............
+
+let name = "aftab"
+let age = 31
+let isLogedIn = flase
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/flase
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
