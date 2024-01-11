03c587ffbf90a3074bbd97f17dccac89a7d9b873
aaftabgit <136881222+aaftabgit@users.noreply.github.com>
2024-01-11 13:13:59 +0000

    string

---
+++ 01_JavaScript-Basics/05_strings.js
@@ -0,0 +1,33 @@
+const name = "aftab"
+const repoCount = 50
+
+// consol.log(name + repoCount + "value")  
+
+//console.log(`hello my name is ${name} and repo count is ${repoCount}`);
+
+const gameName = new String('aftabalam')
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.__proto__)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+ 
+
+const newString = gameName.substring(0, 5)
+//console.log(newString);
+
+const anotherString = gameName.slice(-9, 4)
+//console.log(anotherString);
+
+const newStringOne = "  aftab   "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+const url = "https//afatb.com/alam%20asdf"
+console.log(url.replace('%20', '-'))
+console.log(url.includes('afatb'))
+console.log(url.includes('ghjg'))
+
+// to more informatiom go to 'mdn doc on goooooooogle'
